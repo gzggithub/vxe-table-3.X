86dd3811fe1dd4338c8bd393962fc86dfcfa20fe
郭治国 <gzg@kingyi.net>
2021-01-19 20:18:15 +0800

    测试结构 是  不能从2.X版本直接升级到3.X版本 如果之前没有装过 2.X版本的 可以直接使用最新版本

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'xe-utils'
+import VXETable from 'vxe-table'
+import 'vxe-table/lib/style.css'
+
+Vue.use(VXETable)
+
+// 给 vue 实例挂载内部对象，例如：
+Vue.prototype.$XModal = VXETable.modal
+// Vue.prototype.$XPrint = VXETable.print
+Vue.prototype.$XSaveFile = VXETable.saveFile
+Vue.prototype.$XReadFile = VXETable.readFile
 
 Vue.config.productionTip = false
 
